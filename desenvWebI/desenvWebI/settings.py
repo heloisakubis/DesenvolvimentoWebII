3cf7df90d5aa3de3515acc24bdeff377968f9cc9
Heloisa Marcella Kubis <helokubis@hotmail.com>
2017-11-24 18:12:48 -0200

    Versão 1.5

--- desenvWebI/desenvWebI/settings.py
+++ desenvWebI/desenvWebI/settings.py
@@ -138,10 +138,16 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATICFILES_DIRS = [STATIC_DIR, ]
-STATIC_URL = '/static/'
+# STATICFILES_DIRS = [STATIC_DIR, ]
+# STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 #Media files
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
