2303460b008b2522a0af598fed08dfa00a8e081d
heloisakubis <helokubis@hotmail.com>
2018-06-16 20:42:49 -0300

    Instalação do Sphinx

---
+++ desenvWebI/docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{"\u00edndice":0,"m\u00f3dulo":0,"p\u00e1gina":0,content:0,pesquisa:0},titles:["Welcome to Desenvolvimento Web II&#8217;s documentation!"],titleterms:{desenvolvimento:0,document:0,indic:0,tabl:0,web:0,welcom:0}})
