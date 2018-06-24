31d1e2edd04aed8d07462268bafe5512ac09c3ed
Heloisa Marcella Kubis <helokubis@hotmail.com>
2018-06-24 20:08:06 -0300

    Informações Sphinx

---
+++ desenvWebI/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{"\u00edndice":0,"m\u00f3dulo":0,"p\u00e1gina":0,busca:0},titles:["Welcome to SIFLUX\u2019s documentation!"],titleterms:{document:0,indic:0,siflux:0,tabl:0,welcom:0}})
